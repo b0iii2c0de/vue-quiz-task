4724c67156a726effc4854c363bbf96245cc9a4f
b0iii2c0de <boiiicode@gmail.com>
2024-05-07 21:07:19 +0300

    init: реализовать тестовое задание

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts}",
+  ],
+  theme: {
+    screens: {
+      xs: '320px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontSize: {
+      sm: ['0.7rem', '1'],
+      base: ['1rem', '1.5'],
+      lg: ['1.5rem', '2'],
+      xl: ['2.5rem', '2.5'],
+    },
+    extend: {}
+  },
+  plugins: [],
+}
