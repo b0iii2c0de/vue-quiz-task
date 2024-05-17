a84b9d3fbeb54c726f013c738e2e5551d86b9669
b0iii2c0de <boiiicode@gmail.com>
2024-05-17 19:39:53 +0300

    feat: добавить дополнительные ком-ты

--- src/pages/CommentPage.vue
+++ src/pages/CommentPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button } from '@/shared/ui/button';
+import { Avatar } from '@/shared/ui/avatar';
 
 let greeting = 'Полёт нормальный!';
 let log = () => console.log(greeting);
@@ -7,13 +8,14 @@ let log = () => console.log(greeting);
 </script>
 
 <template>
-  <h1 class="text-slate-900 text-center text-4xl">
+  <h1 class="text-center text-4xl">
     {{ greeting }}
   </h1>
   <Button 
     variant="outline" 
     size="xs" 
-    class="bg-orange-500" 
+    class="bg-orange-500 bg-background text-foreground" 
     @click="log"
   >{{ greeting }}</Button>
+  <Avatar shape="square" size="sm" />
 </template>
