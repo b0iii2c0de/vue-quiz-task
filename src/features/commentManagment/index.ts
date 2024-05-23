311a676cb05b589941bb4f78cea608762cb4c643
b0iii2c0de <boiiicode@gmail.com>
2024-05-23 17:43:30 +0300

    feat: реализовать удаление комментария

--- src/features/commentManagment/index.ts
+++ src/features/commentManagment/index.ts
@@ -1,2 +1,3 @@
 export { default as AddComment } from './ui/AddComment.vue';
 export { default as EditComment } from './ui/EditComment.vue';
+export { default as DeleteComment } from './ui/DeleteComment.vue';
---
+++ src/features/commentManagment/ui/DeleteComment.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import type { Comment } from '@/entities/comment';
+  import { deleteCommentMock } from '@/entities/comment';
+  import { Button } from '@/shared/ui/button';
+
+  const props = defineProps<{ comment: Comment }>();
+
+  const deleteComment = () => {
+    deleteCommentMock(props.comment.id);
+  };
+</script>
+
+<template>
+  <div>
+    <Button @click="deleteComment">Delete Comment</Button>
+  </div>
+</template>  
